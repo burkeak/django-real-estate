f25c4b52593cd0f7dd3ca61791080fbada04cc54
Aloysius Burke <burkeak@gmail.com>
2022-11-11 11:54:06 -0400

    feat: register,login,update , user profile

--- apps/profiles/renderers.py
+++ apps/profiles/renderers.py
@@ -5,7 +5,7 @@ from rest_framework.renderers import JSONRenderer
 
 class ProfileJSONRenderer(JSONRenderer):
     charset = "utf-8"
-
+    # A function that overides the Json renderer to render our in a profile namespace
     def render(self, data, accepted_media_types=None, renderer_context=None):
         errors = data.get("errors", None)
 
